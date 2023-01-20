2386ab2767bcc86279e44acd10d2e8de15e679fa
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-01-20 13:27:52 +0545

    Route for Authentication of Seller has been created

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,16 @@
 import express from "express";
+import {
+  login,
+  register,
+  sellerLogin,
+  sellerRegister,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/seller/register", sellerRegister);
+router.post("/seller/login", sellerLogin);
 
 export default router;
