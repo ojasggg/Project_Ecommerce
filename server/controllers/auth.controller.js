610b429f129537bf3997534f2a5a450fb30071d5
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-01-20 13:26:03 +0545

    Seller Authentication Created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,121 @@
-import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
 import Customer from "../models/customer.model.js";
+import Seller from "../models/seller.model.js";
+import { createError } from "../utils/createError.js";
 
-export const register = (req, res, next) => {
-  const { firstName,lastName,username,email, password, phoneNumber } = req.body;
-  if (!firstName || !lastName || !username || !email || !password || !phoneNumber) {
-    return res.status(400).json(message: "These Fields are required");
+// Customer Authentication
+export const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const { firstName, lastName, username, phoneNumber, email } = req.body;
+    const newCustomer = new Customer({
+      firstName,
+      lastName,
+      username,
+      email,
+      phoneNumber,
+      password: hash,
+    });
+    await newCustomer.save();
+    return res.status(200).json({
+      message: "Registration Successful",
+      newCustomer,
+    });
+  } catch (error) {
+    next(error);
   }
-  
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const customer = await Customer.findOne({ email: req.body.email }).select(
+      "+password"
+    );
+    if (!customer) {
+      return next(createError(404, "Invalid Credentials"));
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      customer.password
+    );
+    if (!isPasswordCorrect) {
+      return next(createError(404, "Invalid Credentials"));
+    }
+
+    const token = jsonwebtoken.sign(
+      { id: customer._id, userType: customer.userType },
+      process.env.SECRET_KEY
+    );
+
+    const { password, userType, ...others } = customer._doc;
+    return res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+      })
+      .status(200)
+      .json({ message: "Logged in successfully ", ...others });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Seller Authentication
+export const sellerRegister = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const { sellerName, sellerEmail, sellerContact } = req.body;
+    const sellerAddress = req.body.sellerAddress;
+    req.body;
+    const newSeller = new Customer({
+      sellerName,
+      sellerEmail,
+      password: hash,
+      sellerContact,
+      sellerAddress,
+    });
+    await newSeller.save();
+    return res.status(200).json({
+      message: "Seller Registration Successful",
+      newSeller,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const sellerLogin = async (req, res, next) => {
+  try {
+    const seller = await Seller.findOne({
+      sellerName: req.body.sellerName,
+    }).select("+password");
+    if (!seller) {
+      next(createError(404, "Invalid Credentials"));
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      seller.password
+    );
+    if (!isPasswordCorrect) {
+      return next(createError(404, "Invalid Credentials"));
+    }
+
+    const token = jsonwebtoken.sign({ id: seller._id }, process.env.SECRET_KEY);
+
+    const { password, ...others } = seller._doc;
+    return res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+      })
+      .status(200)
+      .json({ message: "Seller Logged in successfully ", ...others });
+  } catch (error) {
+    next(error);
+  }
 };
