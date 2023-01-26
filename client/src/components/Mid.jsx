9f2e10f91dd1198dfb46d64a928e72597d92467a
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-01-26 23:22:47 +0545

    Default blank page for profile and setting created

--- client/src/components/Mid.jsx
+++ client/src/components/Mid.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { Cart, Home, IndividualItem, Login, Register } from "../pages";
+import {
+  Cart,
+  Home,
+  IndividualItem,
+  Login,
+  Profile,
+  Register,
+  Setting,
+} from "../pages";
 
 const Mid = () => {
   return (
@@ -10,6 +18,8 @@ const Mid = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/setting" element={<Setting />} />
       <Route path="/item" element={<IndividualItem />} />
     </Routes>
   );
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,3 +3,5 @@ export { default as IndividualItem } from "./IndividualItem";
 export { default as Cart } from "./Cart";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
+export { default as Profile } from "./Profile";
+export { default as Setting } from "./Setting";
