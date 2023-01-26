08fca0d9dfa2ba37f99c168db8d9692a68c79977
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-01-26 23:21:49 +0545

    Passing props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Topbar, Navbar, Mid, Footer } from "./components";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const hasToken = JSON.parse(localStorage.getItem("auth_token"));
+    if (hasToken) setIsAuthenticated(true);
+  });
   return (
     <div className="flex flex-col bg-background font-primary text-black min-h-screen box-border">
       <div className="flex-none">
         <Topbar />
-        <Navbar />
+        <Navbar isAuthenticated={isAuthenticated} />
       </div>
       <div className="grow">
         <Mid />
