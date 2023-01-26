23439d8704e9572b083252f9f46ef195bd2043ce
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-01-26 21:27:07 +0545

    Multiple Routes are added to work with internal routing

--- client/src/components/Mid.jsx
+++ client/src/components/Mid.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { Cart, Home, IndividualItem } from "../pages";
+import { Cart, Home, IndividualItem, Login, Register } from "../pages";
 
 const Mid = () => {
   return (
     <Routes>
-      <Route index element={<Cart />} />
+      <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/item" element={<IndividualItem />} />
     </Routes>
   );
 };
